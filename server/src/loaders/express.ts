b681f67d1323effe688709c75bbcc445210ab017
negu63 <dnjsdn2019@gmail.com>
2021-08-12 01:32:43 +0900

    chore: morgan 패키지 추가

--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -5,11 +5,15 @@ import cors from 'cors';
 import corsOptions from 'config/cors';
 import routes from 'routes';
 
+import morgan from 'morgan';
+
 export default (app: Application) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(cors(corsOptions));
+
+  app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'combined'));
 
   app.use('/api', routes);
 
