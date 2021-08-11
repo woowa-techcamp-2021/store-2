3f74f83012e3ec3836b0a2131021b324ace4f671
negu63 <dnjsdn2019@gmail.com>
2021-08-12 00:43:51 +0900

    chore: .env 클라이언트 오리진 설정

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -1,7 +1,7 @@
 const corsOptions = {
-  origin: process.env.CLIENT_ORIGIN, // 접근 권한을 부여하는 도메인 (Web Server)
-  credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
-  optionsSuccessStatus: 200, // 응답 상태 200으로 설정
+  origin: process.env.CLIENT_ORIGIN,
+  credentials: true,
+  optionsSuccessStatus: 200,
 };
 
 export default corsOptions;
