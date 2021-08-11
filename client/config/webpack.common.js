59e77606dfe7e8042685ef18af7d26960e6bdac0
yoonminsang <apdlyooapdl@naver.com>
2021-08-11 17:27:15 +0900

    feat: 웹팩 적용

---
+++ client/config/webpack.common.js
@@ -0,0 +1,67 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+const __dirname = path.resolve();
+
+export default {
+  entry: './src/index.tsx',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)$/,
+        use: {
+          loader: 'babel-loader',
+        },
+        exclude: /(node_modules)/,
+      },
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        test: /\.(ico|png|jpe?g|gif)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(ttf|otf|svg)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|ico)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'assets/[name].[ext]?[hash]',
+          },
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+};
