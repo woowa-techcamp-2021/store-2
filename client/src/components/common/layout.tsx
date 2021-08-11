c1d851c44d0399c6ffab866ed866d371e70b7ca0
yoonminsang <apdlyooapdl@naver.com>
2021-08-11 17:29:19 +0900

    feat: 예시 코드 작성

---
+++ client/src/components/common/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Layout() {
+  return (
+    <div>
+      <h1>layout</h1>
+      <div>
+        <button>+1</button>
+        <button>-1</button>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
