0ba24cf3881eee781fd900fb8d3f61bfdc264a92
Seogeurim <geulims@naver.com>
2021-08-12 21:36:34 +0900

    feat: 라우터 예제 코드 추가
    App.tsx에 라우터 모듈을 불러와 예제 코드 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import CounterContainer from './containers/counter-container';
+import { BrowserRouter, Switch, Route } from './lib/router';
+import { MainPage, UserPage, NotFoundPage } from './pages';
 
 const App: React.FC = () => {
   return (
-    <>
-      <CounterContainer />
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/user" component={UserPage} />
+        <Route path="/" component={NotFoundPage} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
