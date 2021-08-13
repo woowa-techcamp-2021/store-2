00715d32b35855d4af5c7abbd9f8525087368693
yoonminsang <apdlyooapdl@naver.com>
2021-08-14 04:47:26 +0900

    refacto: 절대경로 수정
    - 절대 경로가 잘못지정되있어서 Link에서 오류가 발생
    - tsconfig-paths-webpack-plugin으로 중복 설정 막음

--- client/src/pages/main-page.tsx
+++ client/src/pages/main-page.tsx
@@ -1,7 +1,15 @@
 import React, { ReactElement } from 'react';
+import { Link } from 'Root/lib/router';
 
 const MainPage = (): ReactElement => {
-  return <div>This is MainPage</div>;
+  return (
+    <div>
+      This is MainPage
+      <Link to="/user">유저</Link>
+      <Link to="/test">테스트</Link>
+      <Link to="/counter">사가 카운터</Link>
+    </div>
+  );
 };
 
 export default MainPage;
