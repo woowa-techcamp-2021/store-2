b48ca5a9c0fd7cba4a1652c3c0ccde58b131338f
yoonminsang <apdlyooapdl@naver.com>
2021-08-14 02:57:02 +0900

    feat: jest 기본 테스트 작성
    - 정말 기본적인 테스트코드

---
+++ client/src/test-example/promise.test.ts
@@ -0,0 +1,29 @@
+interface IId {
+  id: number;
+  name: string;
+  email: string;
+}
+
+function fetchUser(id: number) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('wait 0.1 sec.');
+
+      const user: IId = {
+        id,
+        name: `User${id}`,
+        email: `${id}@test.com`,
+      };
+      resolve(user);
+    }, 100);
+  });
+}
+
+test('fetch a user', async () => {
+  const user = await fetchUser(1);
+  expect(user).toEqual({
+    id: 1,
+    name: 'User1',
+    email: '1@test.com',
+  });
+});
