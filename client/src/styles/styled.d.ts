abf5f70e1b8a30f846948887b544aa7277cf89f8
Seogeurim <geulims@naver.com>
2021-08-14 01:01:56 +0900

    chore: 주석 제거

--- client/src/styles/styled.d.ts
+++ client/src/styles/styled.d.ts
@@ -1,14 +1,13 @@
 import 'styled-components';
 
-// and extend them!
 declare module 'styled-components' {
   export interface DefaultTheme {
     bodyColor: string;
-    // breakpoints
+
     mobile: string;
     tablet: string;
     laptop: string;
-    // font sizes
+
     NORMAL_SIZE: string;
     SMALL_SIZE: string;
   }
--- client/src/styles/theme.tsx
+++ client/src/styles/theme.tsx
@@ -1,19 +1,14 @@
 import React, { FC } from 'react';
-import {
-  ThemeProvider,
-  createGlobalStyle,
-  DefaultTheme,
-} from 'styled-components';
+import { ThemeProvider, createGlobalStyle, DefaultTheme } from 'styled-components';
 import reset from 'styled-reset';
 
 const theme: DefaultTheme = {
-  // colors
   bodyColor: '#626666',
-  // breakpoints
+
   mobile: '(max-width: 480px)',
   tablet: '(max-width: 960px)',
   laptop: '(max-width: 1440px)',
-  // font sizes
+
   NORMAL_SIZE: '16px',
   SMALL_SIZE: '12px',
 };
