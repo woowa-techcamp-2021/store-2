ba236a9d594a1f8f05fe6de69c84504fac5c9c4b
edegiil <electropark917@gmail.com>
2021-08-13 19:26:25 +0900

    feat: 스타일드 컴포넌트 예시 코드 추가

--- client/src/pages/main-page.tsx
+++ client/src/pages/main-page.tsx
@@ -1,7 +1,15 @@
 import React, { ReactElement } from 'react';
 
+import styled from 'styled-components';
+
+const Div = styled.div`
+  padding: 12px;
+  background-color: #a1e0ff;
+  color: ${props => props.theme.bodyColor};
+`;
+
 const MainPage = (): ReactElement => {
-  return <div>This is MainPage</div>;
+  return <Div>This is MainPage</Div>;
 };
 
 export default MainPage;
