0cf18487dd97fb3585ce906a9403e6fdd725ca69
yoonminsang <apdlyooapdl@naver.com>
2021-08-14 02:59:08 +0900

    feat: 테스트 파일 page에 추가

---
+++ client/src/pages/saga-counter-page.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+import CounterContainer from '../containers/counter-container';
+
+const SagaCounterPage = (): ReactElement => {
+  return (
+    <>
+      <CounterContainer />
+    </>
+  );
+};
+
+export default SagaCounterPage;
