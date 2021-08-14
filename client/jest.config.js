7588965f598c4508f5b3c65238bc4073aee370a8
Seogeurim <geulims@naver.com>
2021-08-15 00:23:46 +0900

    chore: 주석 삭제 및 린트 해결

--- client/jest.config.js
+++ client/jest.config.js
@@ -6,9 +6,5 @@ export default {
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/**/*.test.(js|jsx|ts|tsx)'],
   setupFilesAfterEnv: ['<rootDir>/setup-tests.ts'],
-  // testMatch: [
-  //   '<rootDir>/**/*.test.(js|jsx|ts|tsx)',
-  //   '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))',
-  // ],
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
