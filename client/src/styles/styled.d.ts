f520d80c8db36be7bccbd4bd7fa73066f2503970
Seogeurim <geulims@naver.com>
2021-08-15 14:13:53 +0900

    feat: 컬러 전역 변수 설정

--- client/src/styles/styled.d.ts
+++ client/src/styles/styled.d.ts
@@ -2,10 +2,40 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    bodyColor: string;
-
     mobile: string;
     tablet: string;
     laptop: string;
+
+    colorWhite: string;
+    colorOffWhite: string;
+    colorBlack: string;
+    colorSoftBlack: string;
+
+    colorBg: string;
+    colorPlaceholder: string;
+    colorError: string;
+
+    colorLine: string;
+    colorLineLight: string;
+    colorLineDark: string;
+
+    colorTextBrown: string;
+    colorTextBrownLight: string;
+
+    colorPrimary: string;
+    colorPrimaryLight: string;
+    colorPrimaryDark: string;
+
+    colorPointDarkGreen: string;
+    colorPointGreen: string;
+    colorPointBeige: string;
+    colorPointBeigeLight: string;
+    colorPointRed: string;
+    colorPointAqua: string;
+
+    colorGreyMid: string;
+    colorGreyLight: string;
+
+    colorGithub: string;
   }
 }
--- client/src/styles/theme.tsx
+++ client/src/styles/theme.tsx
@@ -3,11 +3,41 @@ import { ThemeProvider, createGlobalStyle, DefaultTheme } from 'styled-component
 import reset from 'styled-reset';
 
 const theme: DefaultTheme = {
-  bodyColor: '#626666',
-
   mobile: '(max-width: 480px)',
   tablet: '(max-width: 960px)',
   laptop: '(max-width: 1440px)',
+
+  colorWhite: '#FFFFFF',
+  colorOffWhite: '#FCFCFC',
+  colorBlack: '#000000',
+  colorSoftBlack: '#333333',
+
+  colorBg: '#EBDFCD',
+  colorPlaceholder: '#7E7E7E',
+  colorError: '#F45452',
+
+  colorLine: '#9F927F',
+  colorLineLight: '#BAA482',
+  colorLineDark: '#665946',
+
+  colorTextBrown: '#665946',
+  colorTextBrownLight: '#F7EAD7',
+
+  colorPrimary: '#2AC1BC',
+  colorPrimaryLight: '#A0E1E0',
+  colorPrimaryDark: '#219A95',
+
+  colorPointDarkGreen: '#11403E',
+  colorPointGreen: '#348011',
+  colorPointBeige: '#BAA482',
+  colorPointBeigeLight: '#F7EAD7',
+  colorPointRed: '#9D3622',
+  colorPointAqua: '#34C6CF',
+
+  colorGreyMid: '#AEAEAE',
+  colorGreyLight: '#D6D6D6',
+
+  colorGithub: '#404040',
 };
 
 const GlobalStyle = createGlobalStyle`
