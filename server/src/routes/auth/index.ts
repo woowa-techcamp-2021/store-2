e6f44536715995ef037982bca06b27a8550f46eb
edegiil <electropark917@gmail.com>
2021-08-16 01:45:40 +0900

    feat: Github OAuth 라우팅

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -4,7 +4,11 @@ import { signIn, checkAuth } from 'controllers/auth';
 import validateToken from 'middlewares/validateToken';
 import { authValidation } from 'validation/auth';
 
+import github from './github';
+
 const router = Router();
+
+router.use('/github', github);
 
 router.post('/', authValidation, signIn);
 router.get('/', checkAuth);
