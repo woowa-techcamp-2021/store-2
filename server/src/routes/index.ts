0af36805c16a35eb4f9d2774393b4c2f311e221e
edegiil <electropark917@gmail.com>
2021-08-15 20:27:09 +0900

    feat: 회원가입 API 라우팅

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
 import auth from './auth';
+import user from './user';
 
 const router = Router();
 
 router.use('/auth', auth);
+router.use('/user', user);
 
 export default router;
--- server/src/routes/user/index.ts
+++ server/src/routes/user/index.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 
-import { signIn } from 'controllers/auth';
+import { signUp } from 'controllers/user';
 import { authValidation } from 'validation/auth';
 
 const router = Router();
 
-router.post('/', authValidation, signIn);
+router.post('/', authValidation, signUp);
 
 export default router;
