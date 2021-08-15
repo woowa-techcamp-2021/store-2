efa9f2d23e5ecc552572582a3d3e5ed2436988e8
edegiil <electropark917@gmail.com>
2021-08-15 16:07:11 +0900

    fix: 테스트 코드 삭제

--- server/src/routes/test/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getTest } from 'controllers/test';
-import testMiddleware from 'middlewares/testMiddleware';
-
-const testRouter = Router();
-
-testRouter.get('/', testMiddleware, getTest);
-
-export default testRouter;
