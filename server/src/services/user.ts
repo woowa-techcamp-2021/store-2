a74de619a3ed3dd57b3c4f9463db0fd501b4e659
edegiil <electropark917@gmail.com>
2021-08-16 01:34:13 +0900

    feat: Github OAuth 컨트롤러 함수

--- server/src/services/user.ts
+++ server/src/services/user.ts
@@ -2,18 +2,34 @@ import { checkUserExists, createUser } from 'repositories/user';
 
 import { createToken } from 'utils/jwt';
 import { hashPassword } from 'utils/crypto';
+import errorGenerator from 'utils/error/error-generator';
 
 interface IToken {
   accessToken: string;
   refreshToken: string;
 }
 
-async function signUp(user_id: string, password: string): Promise<IToken> {
-  await checkUserExists(user_id);
+async function signUp(user_id: string, isOAuth: boolean, password: string): Promise<IToken> {
+  const isUserExists = await checkUserExists(user_id);
+
+  if (isUserExists) {
+    throw errorGenerator({
+      message: 'POST /api/user - account already exists',
+      code: 'auth/existing-user',
+    });
+  }
 
   const hashedPassword = await hashPassword(password);
 
-  const user = await createUser(user_id, hashedPassword);
+  const provider = isOAuth ? 'github' : 'local';
+  const passwordOnDB = isOAuth ? undefined : hashedPassword;
+  const createUserArgs = {
+    user_id,
+    provider,
+    password: passwordOnDB,
+  };
+
+  const user = await createUser(createUserArgs);
 
   const uid = user.getDataValue('id');
 
