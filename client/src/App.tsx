e3352ed98b838cf16447c424075aa96f69c4f0fc
yoonminsang <apdlyooapdl@naver.com>
2021-08-16 00:19:49 +0900

    feat: 로그인 페이지 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'lib/router';
-import { MainPage, UserPage, NotFoundPage, SagaCounterPage, TestPage } from 'pages';
+import { MainPage, UserPage, NotFoundPage, SagaCounterPage, TestPage, SagaAxiosPage, LoginPage } from 'pages';
 import Theme from './styles/theme';
 
 const App: React.FC = () => {
@@ -9,9 +9,11 @@ const App: React.FC = () => {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={MainPage} />
+          <Route exact path="/login" component={LoginPage} />
           <Route exact path="/user" component={UserPage} />
           <Route exact path="/test" component={TestPage} />
           <Route exact path="/counter" component={SagaCounterPage} />
+          <Route exact path="/axios" component={SagaAxiosPage} />
           <Route path="" component={NotFoundPage} />
         </Switch>
       </BrowserRouter>
