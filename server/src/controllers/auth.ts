90aad9fc14ba99df0b97e9f4c264c01805224209
edegiil <electropark917@gmail.com>
2021-08-15 22:10:34 +0900

    feat: 인증 여부 체크 API 컨트롤러 함수

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import authService from 'services/auth';
 
 import errorHandler from 'utils/error/error-handler';
+import { getAccessToken, getRefreshToken } from 'utils/jwt';
 
 interface IReqBody {
   id: string;
@@ -15,10 +16,34 @@ export const signIn = async (req: Request, res: Response): Promise<void> => {
 
     const { accessToken, refreshToken } = await authService.signIn(id, password);
 
-    res.cookie('_rt', refreshToken, { httpOnly: true });
+    res.cookie('rteofkreensh', refreshToken, { httpOnly: true });
     res.status(200).json({ accessToken });
   } catch (err) {
     console.log(err);
+    const { statusCode, errorMessage } = errorHandler(err);
+    res.status(statusCode).json({ errorMessage });
+  }
+};
+
+export const checkAuth = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { redirect } = req.query;
+
+    const accessToken = getAccessToken(req.headers.authorization);
+    const refreshToken = getRefreshToken(req.cookies);
+
+    const { newAccessToken, isAccessTokenExpired } = await authService.checkAuth(accessToken, refreshToken);
+
+    if (redirect) {
+      res.status(200).json({ requestAgain: true, newAccessToken });
+      return;
+    }
+
+    const result = isAccessTokenExpired ? { newAccessToken } : {};
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.clearCookie('_rt');
     const { statusCode, errorMessage } = errorHandler(err);
     res.status(statusCode).json({ errorMessage });
   }
