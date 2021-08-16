1bcc3b05c7acd370321497722e629c1eca421eab
Seogeurim <geulims@naver.com>
2021-08-16 19:31:58 +0900

    feat: 카테고리 데이터 생성
    - 카테고리 데이터 상수 생성
    - 데이터 테이블 생성 후 카테고리 데이터가 추가되도록 설정

--- server/src/models/category.ts
+++ server/src/models/category.ts
@@ -8,17 +8,22 @@ export interface CategoryAttribures {
 export type CategoryCreationAttributes = Optional<CategoryAttribures, 'id'>;
 
 const categorySchema = (sequelize: Sequelize): ModelCtor<Model<CategoryAttribures, CategoryCreationAttributes>> => {
-  const Category = sequelize.define('Category', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: {
+        type: DataTypes.STRING(6),
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+      },
     },
-    name: {
-      type: DataTypes.STRING(45),
-      allowNull: false,
+    {
+      timestamps: false,
     },
-  });
+  );
 
   return Category;
 };
