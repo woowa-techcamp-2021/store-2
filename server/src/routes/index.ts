b22aed1751236b1b3388b237015151cedf1c8026
Seogeurim <geulims@naver.com>
2021-08-16 20:04:57 +0900

    refactor: user 라우터 uri 복수형으로 수정

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
 import auth from './auth';
-import user from './user';
+import users from './users';
 
 const router = Router();
 
 router.use('/auth', auth);
-router.use('/user', user);
+router.use('/users', users);
 
 export default router;
