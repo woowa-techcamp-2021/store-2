c82fdec2085d879fc033651e58250a8924e73eeb
Seogeurim <geulims@naver.com>
2021-08-16 21:24:10 +0900

    feat: 모달 컴포넌트 수정 및 반응형 스타일링
    - 모달 컴포넌트 재사용성을 위한 수정
    - 모달 컴포넌트 반응형 스타일링

--- client/src/components/auth/auth-success-modal.tsx
+++ client/src/components/auth/auth-success-modal.tsx
@@ -1,71 +1,48 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
-import Modal from 'components/common/modal';
-import congrats from 'assets/icons/congrats.gif';
+import { Modal } from 'components';
 
-const Inner = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  h2 {
-    font-family: ${props => props.theme.fontEuljiro};
-  }
-  div {
-    font-family: ${props => props.theme.fontHannaAir};
-  }
-  span {
-    color: ${props => props.theme.colorPrimary};
-  }
-  b {
-    font-weight: 600;
-  }
-  ${({ theme }) => theme.mobile} {
-    h2 {
-    }
-    div {
-    }
-  }
-  ${({ theme }) => theme.tablet} {
-    h2 {
-    }
-    div {
-    }
-  }
-  ${({ theme }) => theme.laptop} {
-    h2 {
-      font-size: 50px;
-      margin: 30px;
-    }
-    div {
-      font-size: 25px;
-    }
-  }
-`;
-
-const Img = styled.img`
-  margin-top: 90;
-  margin-bottom: 30;
-  width: 250px;
-`;
-
-interface Inf {
+interface AuthSuccessModalProps {
   userId: string | null | undefined;
 }
 
-const AuthSuccessModal: FC<Inf> = ({ userId }) => {
+const Title = styled.h2`
+  span {
+    color: ${props => props.theme.colorPrimary};
+  }
+`;
+
+const Info = styled.div`
+  p {
+    margin-bottom: 3px;
+  }
+
+  span {
+    font-family: ${props => props.theme.fontHanna};
+    font-size: 105%;
+    vertical-align: middle;
+    margin-right: 1px;
+  }
+`;
+
+const AuthSuccessModal: FC<AuthSuccessModalProps> = ({ userId }) => {
   return (
-    <Modal>
-      <Img src={congrats} alt="축하" />
-      <Inner>
-        <h2>
-          회원가입이 <span>완료</span>되었습니다
-        </h2>
-        <div>
-          <b>{userId}</b>님의 회원가입을 축하드립니다
-        </div>
-        <div>알차고 실속있는 서비스로 찾아뵙겠습니다</div>
-      </Inner>
-    </Modal>
+    <Modal
+      type="alert"
+      header={
+        <Title>
+          회원가입이 <span>완료</span>되었습니다.
+        </Title>
+      }
+      body={
+        <Info>
+          <p>
+            <span>{userId || 'guest'}</span>님의 회원가입을 축하합니다.
+          </p>
+          <p>알차고 실속있는 서비스로 찾아뵙겠습니다.</p>
+        </Info>
+      }
+    />
   );
 };
 
