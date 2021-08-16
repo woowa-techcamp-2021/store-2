2a5bba41f4491f4125005eb16eed9e1b83257158
yoonminsang <apdlyooapdl@naver.com>
2021-08-17 03:10:08 +0900

    wip: 헤더가 바뀌면서 테스트 코드 수정

--- client/src/components/common/__test__/header.test.tsx
+++ client/src/components/common/__test__/header.test.tsx
@@ -4,22 +4,22 @@ import Header from '../header';
 
 describe('<Header />', () => {
   it('matches snapshot pc main', () => {
-    const { container } = render(<Header displayMain isMobile={false} />);
+    const { container } = render(<Header displayMain isMobile={false} userId="test" onLogout={() => null} />);
     expect(container).toMatchSnapshot();
   });
 
   it('matches snapshot pc', () => {
-    const { container } = render(<Header displayMain={false} isMobile={false} />);
+    const { container } = render(<Header displayMain={false} isMobile={false} userId="test" onLogout={() => null} />);
     expect(container).toMatchSnapshot();
   });
 
   it('matches snapshot mobile main', () => {
-    const { container } = render(<Header displayMain isMobile />);
+    const { container } = render(<Header displayMain isMobile userId="test" onLogout={() => null} />);
     expect(container).toMatchSnapshot();
   });
 
   it('matches snapshot mobile', () => {
-    const { container } = render(<Header displayMain={false} isMobile />);
+    const { container } = render(<Header displayMain={false} isMobile userId="test" onLogout={() => null} />);
     expect(container).toMatchSnapshot();
   });
 });
--- client/src/components/common/__test__/navbar.test.tsx
+++ client/src/components/common/__test__/navbar.test.tsx
@@ -4,12 +4,12 @@ import Navbar from '../navbar';
 
 describe('<Navbar />', () => {
   it('matches snapshot', () => {
-    const { container } = render(<Navbar white />);
+    const { container } = render(<Navbar white userId="test" onLogout={() => null} />);
     expect(container).toMatchSnapshot();
   });
 
   it('matches snapshot mobile', () => {
-    const { container } = render(<Navbar mobile />);
+    const { container } = render(<Navbar mobile userId="test" onLogout={() => null} />);
     expect(container).toMatchSnapshot();
   });
 });
