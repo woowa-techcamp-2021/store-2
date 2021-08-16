2b1b548fc0be9871d64fc39d1ec957c43d082e98
yoonminsang <apdlyooapdl@naver.com>
2021-08-16 17:08:39 +0900

    feat: 회원가입 ui 및 기본 validation 구현, 코드 재활용
    - 로그인과 같은 컴폰너트를 사용하고 삼항연산자로 필요한 부분만 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'lib/router';
-import { MainPage, UserPage, NotFoundPage, SagaCounterPage, TestPage, SagaAxiosPage, LoginPage } from 'pages';
+import {
+  MainPage,
+  UserPage,
+  NotFoundPage,
+  SagaCounterPage,
+  TestPage,
+  SagaAxiosPage,
+  LoginPage,
+  SignupPage,
+} from 'pages';
 import Theme from './styles/theme';
 
 const App: React.FC = () => {
@@ -10,6 +19,7 @@ const App: React.FC = () => {
         <Switch>
           <Route exact path="/" component={MainPage} />
           <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/signup" component={SignupPage} />
           <Route exact path="/user" component={UserPage} />
           <Route exact path="/test" component={TestPage} />
           <Route exact path="/counter" component={SagaCounterPage} />
--- client/src/components/auth/auth-form.tsx
+++ client/src/components/auth/auth-form.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 import baedal from 'assets/icons/baedalee.png';
 import github from 'assets/icons/github.png';
 import { useHistory } from 'lib/router';
-import LoginButton from './login-button';
-import LoginInput from './login-input';
+import AuthButton from './auth-button';
+import AuthInput from './auth-input';
 
 const Div = styled.div`
   display: flex;
@@ -44,31 +44,56 @@ const Image = styled.img`
 const Error = styled.div`
   color: ${props => props.theme.colorError};
   margin-top: 20px;
+  margin-bottom: 10px;
   display: flex;
   text-indent: 5px;
 `;
 
-interface LoginProps {
+const CheckDiv = styled.div`
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  font-weight: 600;
+  div {
+    margin-left: 10px;
+  }
+`;
+
+interface AuthProps {
   id: string;
   password: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onSubmit: (e: React.FormEvent) => void;
   error: string | null;
   loading: boolean;
+  isSignup?: boolean;
+  check?: boolean;
+  onCheckChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const LoginForm: FC<LoginProps> = ({ id, password, onChange, onSubmit, error, loading }) => {
+const AuthForm: FC<AuthProps> = ({
+  id,
+  password,
+  onChange,
+  onSubmit,
+  error,
+  loading,
+  isSignup,
+  check,
+  onCheckChange,
+}) => {
   const history = useHistory();
   const onClick = useCallback(() => {
     history.push('/signup');
   }, [history]);
+  const TEXT = isSignup ? '회원가입' : '로그인';
 
   return (
     <Div>
       <Form onSubmit={onSubmit}>
         {loading && <div>로딩중~~</div>}
-        <LoginInput type="text" placeholder="아이디" value={id} name="id" onChange={onChange} maxLength={10} />
-        <LoginInput
+        <AuthInput type="text" placeholder="아이디" value={id} name="id" onChange={onChange} maxLength={10} />
+        <AuthInput
           type="password"
           placeholder="비밀번호"
           value={password}
@@ -76,22 +101,39 @@ const LoginForm: FC<LoginProps> = ({ id, password, onChange, onSubmit, error, lo
           onChange={onChange}
           maxLength={14}
         />
+        {isSignup && (
+          <CheckDiv>
+            <input type="checkbox" checked={check} onChange={onCheckChange} />
+            <div>배민문방구 전체 동의</div>
+          </CheckDiv>
+        )}
+
         <Error>{error}</Error>
-        <LoginButton type="submit" login>
+        <AuthButton type="submit" login>
           <Image src={baedal} alt="배달이" />
-          로그인
-        </LoginButton>
+          {TEXT}
+        </AuthButton>
       </Form>
-      <LoginButton type="button" github>
-        <Image src={github} alt="배달이" />
-        깃-헙으로 로그인
-      </LoginButton>
-      <LoginButton type="button" signup onClick={onClick}>
-        <Image src={baedal} alt="배달이" />
-        배민문구사로 회원가입
-      </LoginButton>
+
+      {isSignup ? (
+        <AuthButton type="button" back>
+          <Image src={baedal} alt="배달이" />
+          로그인 하러가기
+        </AuthButton>
+      ) : (
+        <>
+          <AuthButton type="button" github>
+            <Image src={github} alt="배달이" />
+            깃-헙으로 로그인
+          </AuthButton>
+          <AuthButton type="button" signup onClick={onClick}>
+            <Image src={baedal} alt="배달이" />
+            배민문구사로 회원가입
+          </AuthButton>
+        </>
+      )}
     </Div>
   );
 };
 
-export default LoginForm;
+export default AuthForm;
