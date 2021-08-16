35ddeff6641f1d0411f358e696b9f681ffe456b2
yoonminsang <apdlyooapdl@naver.com>
2021-08-17 03:08:57 +0900

    feat: 로그아웃 및 자동로그인 구현

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,16 +1,15 @@
 import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 import { authReducer, authSaga } from './auth';
-import { axiosReducer, axiosSaga } from './axios';
 import { counterReducer, counterSaga } from './counter';
 
-const rootReducer = combineReducers({ counter: counterReducer, axios: axiosReducer, auth: authReducer });
+const rootReducer = combineReducers({ counter: counterReducer, auth: authReducer });
 
 export type RootState = ReturnType<typeof rootReducer>;
 
 export function* rootSaga(): Generator {
   try {
-    yield all([counterSaga(), axiosSaga(), authSaga()]);
+    yield all([counterSaga(), authSaga()]);
   } catch (e) {
     throw new Error(e);
   }
