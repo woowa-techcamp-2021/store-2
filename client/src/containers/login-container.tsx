49deda706f3a8249429201209e6b2828c859d480
yoonminsang <apdlyooapdl@naver.com>
2021-08-16 12:00:35 +0900

    feat: 로그인 컨테이너 자잘한 수정 및 history 이동
    - 로그인 성공 또는 유저 존재시(로그인되어있는데 url로 이동하는경우) 메인경로로 이동
    - 처음에 유저 체크하는 부분은 안만들어서 100퍼 완성은 아님(테스트해보면 안되는게 서버코드가 바뀜)
    - 문자 상수화
    - useselector 인터페이스 재할당. 기존의 인터페이스를 가져오면 저장소가 커질수록 불필요한 부분을 가져와야되서 수정

--- client/src/containers/login-container.tsx
+++ client/src/containers/login-container.tsx
@@ -3,25 +3,44 @@ import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'store';
 import LoginForm from 'components/auth/login-form';
 import useInputs from 'hooks/use-inputs';
-import { getLogin, StateProps } from 'store/auth';
+import { getLogin } from 'store/auth';
+import { useHistory } from 'lib/router';
+
+interface IRedux {
+  loading: boolean;
+  error: null | string;
+  userId: string | null | undefined;
+}
+
+const ID_ERROR = '아이디를 입력하세요';
+const PWD_ERROR = '비밀번호를 입력하세요';
 
 const LoginContainer: FC = () => {
+  const history = useHistory();
   const [{ id, password }, onChange] = useInputs({ id: '', password: '' });
   const [authError, setAuthError] = useState<null | string>(null);
 
-  const { login }: StateProps = useSelector(({ auth }: RootState) => ({
-    login: auth.login,
+  const { loading, error, userId }: IRedux = useSelector(({ auth }: RootState) => ({
+    loading: auth.login.loading,
+    error: auth.login.error,
+    userId: auth.user.userId,
   }));
   const dispatch = useDispatch();
 
   useEffect(() => {
-    setAuthError(login.error);
-  }, [login.error]);
+    setAuthError(error);
+  }, [error]);
+
+  useEffect(() => {
+    if (userId) {
+      history.push('/');
+    }
+  }, [userId, history]);
 
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (!id) setAuthError('아이디를 입력하세요');
-    else if (!password) setAuthError('비밀번호를 입력하세요');
+    if (!id) setAuthError(ID_ERROR);
+    else if (!password) setAuthError(PWD_ERROR);
     else {
       dispatch({ type: getLogin.type, payload: { id, password } });
     }
@@ -34,7 +53,7 @@ const LoginContainer: FC = () => {
       onChange={onChange}
       onSubmit={onSubmit}
       error={authError}
-      loading={login.loading}
+      loading={loading}
     />
   );
 };
