ad5cd5fcc9be9229f7de769e6a5e115f6e1b0eaa
yoonminsang <apdlyooapdl@naver.com>
2021-08-16 20:20:34 +0900

    wip: 모달 구현중 --
    - 그림님 파이팅!!

--- client/src/components/common/modal.tsx
+++ client/src/components/common/modal.tsx
@@ -9,12 +9,37 @@ const ModalBlock = styled.div`
   height: 100%;
   background: rgba(0, 0, 0, 0.3);
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
 
+const Inner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${props => props.theme.colorWhite};
+  ${({ theme }) => theme.mobile} {
+    border-radius: 40px;
+    /* width: 100%;
+    height: 100%; */
+    margin: 100px 20px;
+  }
+  ${({ theme }) => theme.tablet} {
+  }
+  ${({ theme }) => theme.laptop} {
+    border-radius: 67px;
+    width: 650px;
+    height: 500px;
+  }
+`;
+
 const Modal: FC = ({ children }) => {
-  return <ModalBlock>{children}</ModalBlock>;
+  return (
+    <ModalBlock>
+      <Inner>{children}</Inner>
+    </ModalBlock>
+  );
 };
 
 export default Modal;
