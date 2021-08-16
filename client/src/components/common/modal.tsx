fbb99f2caa2403374c69677390a476f0bc9ce72d
Seogeurim <geulims@naver.com>
2021-08-16 21:24:10 +0900

    feat: 모달 컴포넌트 수정 및 반응형 스타일링
    - 모달 컴포넌트 재사용성을 위한 수정
    - 모달 컴포넌트 반응형 스타일링

--- client/src/components/common/modal.tsx
+++ client/src/components/common/modal.tsx
@@ -1,5 +1,14 @@
-import React, { FC } from 'react';
+import React, { FC, ReactNode } from 'react';
 import styled from 'styled-components';
+
+import alertImg from 'assets/icons/congrats.gif';
+import confirmImg from 'assets/icons/wait.png';
+
+interface ModalProps {
+  type: 'alert' | 'confirm';
+  header?: ReactNode;
+  body?: ReactNode;
+}
 
 const ModalBlock = styled.div`
   position: fixed;
@@ -7,37 +16,103 @@ const ModalBlock = styled.div`
   top: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.3);
+  background-color: rgba(0, 0, 0, 0.3);
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
+  align-items: center;
 `;
 
 const Inner = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
   background-color: ${props => props.theme.colorWhite};
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 50px;
+  height: 500px;
+
   ${({ theme }) => theme.mobile} {
     border-radius: 40px;
-    /* width: 100%;
-    height: 100%; */
-    margin: 100px 20px;
+    width: 90%;
+    max-height: 90%;
   }
+
   ${({ theme }) => theme.tablet} {
-  }
-  ${({ theme }) => theme.laptop} {
-    border-radius: 67px;
     width: 650px;
-    height: 500px;
+    max-width: 90%;
+  }
+
+  ${({ theme }) => theme.laptop} {
+    width: 650px;
   }
 `;
 
-const Modal: FC = ({ children }) => {
+const ModalHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+
+  img {
+    width: 200px;
+    margin-bottom: 20px;
+
+    ${({ theme }) => theme.mobile} {
+      width: 150px;
+    }
+  }
+
+  div {
+    color: ${props => props.theme.colorBlack};
+    font-family: ${props => props.theme.fontEuljiro};
+    font-size: 36px;
+
+    ${({ theme }) => theme.mobile} {
+      font-size: 24px;
+    }
+
+    ${({ theme }) => theme.tablet} {
+      font-size: 32px;
+    }
+  }
+`;
+
+const ModalBody = styled.div`
+  text-align: center;
+  margin-bottom: 20px;
+
+  div {
+    color: ${props => props.theme.colorBlack};
+    font-family: ${props => props.theme.fontHannaAir};
+    font-size: 20px;
+
+    ${({ theme }) => theme.mobile} {
+      font-size: 16px;
+    }
+
+    ${({ theme }) => theme.tablet} {
+      font-size: 18px;
+    }
+  }
+`;
+
+const ModalFooter = styled.div``;
+
+const Modal: FC<ModalProps> = ({ type, header, body }) => {
   return (
     <ModalBlock>
-      <Inner>{children}</Inner>
+      <Inner>
+        <ModalHeader>
+          <img src={type === 'alert' ? alertImg : confirmImg} alt="modal-img" />
+          <div>{header}</div>
+        </ModalHeader>
+        <ModalBody>
+          <div>{body}</div>
+        </ModalBody>
+        {type === 'confirm' && <ModalFooter>buttons</ModalFooter>}
+      </Inner>
     </ModalBlock>
   );
 };
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Logo } from './common/logo';
 export { default as Navbar } from './common/navbar';
+export { default as Modal } from './common/modal';
 
 export { default as Layout } from './layout';
