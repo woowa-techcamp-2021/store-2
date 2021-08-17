e38055f1702bc2241f290636d8b558aba195f745
yoonminsang <apdlyooapdl@naver.com>
2021-08-18 02:58:15 +0900

    fix: oauth 전용 페이지
    - 빈화면인데 뭘 넣어줘도 괜찮을듯

---
+++ client/src/pages/auth-page.tsx
@@ -0,0 +1,23 @@
+import { useHistory } from 'lib/router';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from 'store';
+import { getGithubLogin } from 'store/auth';
+import { IUser } from 'types/auth';
+
+const AuthPage = () => {
+  const { userId }: IUser = useSelector(({ auth }: RootState) => ({
+    userId: auth.user.userId,
+  }));
+  const dispatch = useDispatch();
+  const history = useHistory();
+  useEffect(() => {
+    dispatch({ type: getGithubLogin.type, payload: { code: window.location.search.slice(22) } });
+  }, [dispatch]);
+  useEffect(() => {
+    if (userId) history.push('/');
+  }, [userId, history]);
+  return null;
+};
+
+export default AuthPage;
