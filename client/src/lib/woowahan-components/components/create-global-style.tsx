936c27cf63eeec9b63a7867a7c03911b6dfcfe71
edegiil <electropark917@gmail.com>
2021-08-18 00:22:11 +0900

    fix: css-in-js 스타일 적용 에러 해결

--- client/src/lib/woowahan-components/components/create-global-style.tsx
+++ client/src/lib/woowahan-components/components/create-global-style.tsx
@@ -1,20 +1,25 @@
 import React, { FC } from 'react';
-import { compile, serialize, stringify } from 'stylis';
+import { compile, serialize, stringify, middleware } from 'stylis';
 
 import parseString from '../utils/parse-string';
-import splitStyleString from '../utils/split-style-string';
 
 const createGlobalStyle = (styleString: TemplateStringsArray, ...exps: string[]): FC => {
   const FunctionComponent: FC = () => {
     const parsedString = parseString(styleString, exps, {});
 
-    const preprocessedStyle = serialize(compile(parsedString), stringify);
-    const styleList = splitStyleString(preprocessedStyle);
+    const preprocessedStyle = serialize(
+      compile(parsedString),
+      middleware([element => (element.parent === null ? '\n' : ''), stringify]),
+    ).trim();
+
+    const styleList = preprocessedStyle.split('\n');
 
     const styleSheet = document.styleSheets[0];
 
     styleList.forEach(style => {
-      styleSheet.insertRule(style, styleSheet.cssRules.length);
+      if (style) {
+        styleSheet.insertRule(style, styleSheet.cssRules.length);
+      }
     });
 
     const ReactElement = React.createElement(React.Fragment, null, null);
