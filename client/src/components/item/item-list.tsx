148feb9bea032b278a74f6068d4ca38046930283
yoonminsang <apdlyooapdl@naver.com>
2021-08-19 03:44:59 +0900

    wip: 리스트가 존재하지 않을때 보여주지 않는 경우 추가

--- client/src/components/item/item-list.tsx
+++ client/src/components/item/item-list.tsx
@@ -5,7 +5,7 @@ import { IItem } from 'types/item';
 import Item from 'components/item';
 
 interface ItemListProps {
-  items: IItem[];
+  items: IItem[] | null;
   isLoading: boolean;
 }
 
@@ -34,23 +34,24 @@ const ItemList: FC<ItemListProps> = ({ items }) => {
 
   return (
     <Wrapper>
-      {items.map(item => {
-        return (
-          <Item
-            key={item.id}
-            thumbnail={item.thumbnail}
-            title={item.title}
-            price={item.price}
-            isBest={item.isBest}
-            isGreen={item.isGreen}
-            isNew={item.isNew}
-            isSale={item.isSale}
-            salePercent={item.salePercent}
-            originalPrice={item.originalPrice}
-            onClick={goDetailPage(item.id)}
-          />
-        );
-      })}
+      {items &&
+        items.map(item => {
+          return (
+            <Item
+              key={item.id}
+              thumbnail={item.thumbnail}
+              title={item.title}
+              price={item.price}
+              isBest={item.isBest}
+              isGreen={item.isGreen}
+              isNew={item.isNew}
+              isSale={item.isSale}
+              salePercent={item.salePercent}
+              originalPrice={item.originalPrice}
+              onClick={goDetailPage(item.id)}
+            />
+          );
+        })}
     </Wrapper>
   );
 };
