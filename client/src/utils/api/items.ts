210aff50e96b30c5668a57de67022b592fbb30d8
yoonminsang <apdlyooapdl@naver.com>
2021-08-19 16:48:52 +0900

    wip: api 없는 타입 추가

--- client/src/utils/api/items.ts
+++ client/src/utils/api/items.ts
@@ -1,8 +1,9 @@
 import { AxiosResponse } from 'axios';
+import { IItem } from 'types/item';
 import { IItemsState } from 'store/items';
 import client from './client';
 
 export const getMainItems = (): Promise<AxiosResponse> => client.get('/api/items/main');
 
 export const getCategoryItems = ({ categoryId, pageId = 1, type }: IItemsState): Promise<AxiosResponse> =>
-  client.get(`/api/items/category?categoryId=${categoryId}&pageId=${pageId}&type=${type}`);
+  client.get<IItem>(`/api/items/category?categoryId=${categoryId}&pageId=${pageId}&type=${type}`);
