2cf81319dab3608632cc2619bf767da45ef70915
yoonminsang <apdlyooapdl@naver.com>
2021-08-19 16:54:45 +0900

    feat: 카테고리 아이템 컴포넌트 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'lib/router';
 
-import { MainPage, NotFoundPage, LoginPage, SignupPage, AuthPage, ItemDetailPage } from 'pages';
+import { MainPage, NotFoundPage, LoginPage, SignupPage, AuthPage, ItemDetailPage, CategoryPage } from 'pages';
 import Theme from './styles/theme';
 
 const App: React.FC = () => {
@@ -13,6 +13,7 @@ const App: React.FC = () => {
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/signup" component={SignupPage} />
           <Route path="/item/:id" component={ItemDetailPage} />
+          <Route exact path="/category" component={CategoryPage} />
           <Route exact path="/auth" component={AuthPage} />
           <Route path="" component={NotFoundPage} />
         </Switch>
