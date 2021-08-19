2cfbab10f51eb229339e22cb9ab9c718eff1f59c
yoonminsang <apdlyooapdl@naver.com>
2021-08-19 13:12:42 +0900

    feat: api 카테고리 아이템 불러오기

--- client/src/utils/api/items.ts
+++ client/src/utils/api/items.ts
@@ -1,4 +1,8 @@
 import { AxiosResponse } from 'axios';
+import { IItemsState } from 'store/items';
 import client from './client';
 
 export const getMainItems = (): Promise<AxiosResponse> => client.get('/api/items/main');
+
+export const getCategoryItems = ({ categoryId, pageId = 1, type }: IItemsState): Promise<AxiosResponse> =>
+  client.get(`/api/items/category?categoryId=${categoryId}&pageId=${pageId}&type=${type}`);
