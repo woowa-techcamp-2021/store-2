a6e3c26bdbd3cdf858dabaa9d257995cf623dc2b
yoonminsang <apdlyooapdl@naver.com>
2021-08-19 13:15:08 +0900

    wip: 메인아이템 컨테이너 수정

--- client/src/containers/main-item-container.tsx
+++ client/src/containers/main-item-container.tsx
@@ -1,32 +1,23 @@
 import React, { FC, useEffect } from 'react';
-import ItemList from 'components/item/item-list';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'store';
 import { getMainItems } from 'store/items';
+import MainItems from 'components/item/main-items';
 
 const MainItemContainer: FC = () => {
   const { popularItems, newItems, recommendItems, loading } = useSelector(({ items, loading }: RootState) => ({
     popularItems: items.mainItems.popularItems,
     newItems: items.mainItems.newItems,
     recommendItems: items.mainItems.recommendItems,
-    loading: loading['auth/getMainItemSuccess'],
+    loading: loading['auth/getMainItem'],
   }));
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch({ type: getMainItems.type });
   }, [dispatch]);
   return (
-    <>
-      <div>잘나가요</div>
-      <ItemList items={popularItems} isLoading={loading} />
-      <div>새로 나왔어요</div>
-      <ItemList items={newItems} isLoading={loading} />
-      <div>추천 드려요</div>
-      <ItemList items={recommendItems} isLoading={loading} />
-    </>
+    <MainItems popularItems={popularItems} newItems={newItems} recommendItems={recommendItems} loading={loading} />
   );
-  // 로딩을 위에서나 전체 레이아웃같은데다가 줘도 될듯
-  // return <ItemList items={ITEM_DUMMY} isLoading={false} />;
 };
 
 export default MainItemContainer;
