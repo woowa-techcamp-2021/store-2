8a86e6a7de590809ec45630e9165a5478d183158
yoonminsang <apdlyooapdl@naver.com>
2021-08-19 21:59:35 +0900

    feat: 아이템 검색 서비스 계층 작성.
    - 카테고리와 검색 합치기

--- server/src/services/items.ts
+++ server/src/services/items.ts
@@ -2,6 +2,7 @@ import itemRepository from 'repositories/items';
 import { ItemAttributes, ItemCreationAttributes } from 'models/item';
 import { Model } from 'sequelize';
 import errorGenerator from 'utils/error/error-generator';
+import { getRegExp, engToKor } from 'korean-regexp';
 
 async function mainItems(): Promise<Model<ItemAttributes, ItemCreationAttributes>[][]> {
   const items = await Promise.all([
@@ -13,23 +14,24 @@ async function mainItems(): Promise<Model<ItemAttributes, ItemCreationAttributes
   return items;
 }
 
-async function categoryItems(
+async function getItems(
   categoryId: string,
   pageId = 1,
   type: string,
+  search: string,
 ): Promise<Model<ItemAttributes, ItemCreationAttributes>[]> {
   if (
-    !(
-      categoryId ||
-      type ||
-      categoryId.length === 6 ||
-      ['recommend', 'popular', 'recent ', 'cheap', 'expensive'].includes(type)
-    )
-  )
+    (categoryId && search) ||
+    (!categoryId && !search) ||
+    (categoryId && categoryId.length !== 6) ||
+    Number.isNaN(pageId)
+  ) {
     throw errorGenerator({
       message: 'POST /api/item - no exist querystring',
       code: 'item/no-exist-querystring',
     });
+  }
+
   const order = [];
   //  recommend 수정 예정
   if (type === 'recommend') order.push(['sale_count', 'DESC']);
@@ -37,12 +39,27 @@ async function categoryItems(
   else if (type === 'recent') order.push(['updatedAt', 'DESC']);
   else if (type === 'cheap') order.push(['price', 'ASC']);
   else if (type === 'expensive') order.push(['price', 'DESC']);
-  const categoryReg = categoryId.slice(2, 4) === '00' ? categoryId.slice(0, 2) : categoryId.slice(0, 4);
-  const items = await itemRepository.getCategoryItems(pageId, order, categoryReg);
+
+  let items;
+  if (categoryId) {
+    let categoryReg = '';
+    if (categoryReg) {
+      if (categoryId.slice(2, 4) === '00') categoryReg = categoryId.slice(0, 2);
+      else categoryId.slice(0, 4);
+    }
+    items = await itemRepository.getCategoryItems(pageId, order, categoryReg);
+  } else {
+    const regExp = String(
+      getRegExp(engToKor(search), {
+        initialSearch: true,
+      }),
+    );
+    items = await itemRepository.getSearchItems(pageId, order, regExp.substring(0, regExp.length - 2).slice(1));
+  }
   return items;
 }
 
 export default {
   mainItems,
-  categoryItems,
+  getItems,
 };
