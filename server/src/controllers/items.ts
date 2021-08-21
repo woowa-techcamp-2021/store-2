e5dbed7ee334a3871d0525a16ea6496187ed38f2
yoonminsang <apdlyooapdl@naver.com>
2021-08-21 17:53:51 +0900

    feat: 컨트롤러에 아이템리스트 페이지 카운트 추가

--- server/src/controllers/items.ts
+++ server/src/controllers/items.ts
@@ -13,8 +13,10 @@ interface IQuery {
 
 export const getMainItems = async (req: Request, res: Response): Promise<void> => {
   try {
-    const [popularItems, newItems, recommendItems] = await itemService.mainItems();
-    res.status(200).json({ popularItems, newItems, recommendItems });
+    const { popularItems, newItems, recommendItems } = await itemService.mainItems();
+    res
+      .status(200)
+      .json({ popularItems: popularItems.items, newItems: newItems.items, recommendItems: recommendItems.items });
   } catch (err) {
     console.log(err);
     const { statusCode, errorMessage } = errorHandler(err);
@@ -25,8 +27,9 @@ export const getMainItems = async (req: Request, res: Response): Promise<void> =
 export const getItems = async (req: Request<unknown, unknown, unknown, IQuery>, res: Response): Promise<void> => {
   const { categoryId, pageId, type, search } = req.query;
   try {
-    const items = await itemService.getItems(categoryId, pageId, type, search);
-    res.status(200).json(items);
+    const data = await itemService.getItems(categoryId, pageId, type, search);
+    const { items, pageCount } = data;
+    res.status(200).json({ items, pageCount });
   } catch (err) {
     console.log(err);
     const { statusCode, errorMessage } = errorHandler(err);
