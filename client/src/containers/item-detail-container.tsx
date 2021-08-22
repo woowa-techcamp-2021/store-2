572549b57116434e8c617c55ebcfee9be68f0edb
edegiil <electropark917@gmail.com>
2021-08-22 22:08:22 +0900

    feat: 아이템 상세정보 API 컨테이너 연동

--- client/src/containers/item-detail-container.tsx
+++ client/src/containers/item-detail-container.tsx
@@ -1,27 +1,30 @@
-import React, { FC } from 'react';
+import React, { useEffect, FC } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'lib/router';
 
 import ItemDetail from 'components/item-detail';
 
-const dataFromServer = {
-  thumbnail: 'https://storage.googleapis.com/bmart-5482b.appspot.com/008/302_main_018.png',
-  title: '포스터. 메이배달이',
-  price: 4000,
-  contents: [
-    'https://storage.googleapis.com/bmart-5482b.appspot.com/008/210617/0ce61799bd0ed71aeccd8eb40afb38e5_174140.jpg',
-    'https://storage.googleapis.com/bmart-5482b.appspot.com/008/210617/7702063f6d06989b57bc0b44b6f0a7d3_174146.jpg',
-    'https://storage.googleapis.com/bmart-5482b.appspot.com/008/210617/db9b020d10a4a3d1367948513e57ad28_174152.jpg',
-    'https://storage.googleapis.com/bmart-5482b.appspot.com/008/210617/37d51419b6707a3d8a2b84a866b02e80_174159.jpg',
-    'https://storage.googleapis.com/bmart-5482b.appspot.com/008/210617/adea4720b3c8db7dd508e19b5b33a495_174207.jpg',
-    'https://storage.googleapis.com/bmart-5482b.appspot.com/008/210617/e5d9380fabf06792f9d69b911d8a2afe_174217.jpg',
-    'https://storage.googleapis.com/bmart-5482b.appspot.com/008/210617/779e5e6bfa29605e41990aa9d9763d3f_174233.jpg',
-  ],
-  isLike: false,
-  isSoldOut: true,
-  reviewCount: 2,
-};
+import { RootState } from 'store';
+import { getItem } from 'store/items';
 
 const MainItemContainer: FC = () => {
-  const { thumbnail, title, price, contents, isLike, isSoldOut, reviewCount } = dataFromServer;
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  const { thumbnail, title, price, contents, isLike, isSoldOut, reviewCount } = useSelector(({ items }: RootState) => ({
+    thumbnail: items.item.thumbnail,
+    title: items.item.title,
+    price: items.item.price,
+    contents: items.item.contents,
+    salePercent: items.item.salePercent,
+    isLike: items.item.isLike,
+    isSoldOut: items.item.isSoldOut,
+    reviewCount: items.item.reviewCount,
+  }));
+
+  useEffect(() => {
+    dispatch({ type: getItem.type, payload: { id } });
+  }, [id, dispatch]);
 
   return (
     <ItemDetail
