e253f0e7ed3c3dab699bb4deb34a0337709686fb
edegiil <electropark917@gmail.com>
2021-08-22 16:19:07 +0900

    feat: 버튼 컴포넌트

---
+++ client/src/components/common/button.tsx
@@ -0,0 +1,62 @@
+import React, { FC } from 'react';
+import styled from 'lib/woowahan-components';
+
+type StyleType = 'black' | 'white';
+type ButtonType = 'button' | 'submit' | 'reset';
+type Size = 'big' | 'small';
+
+interface ButtonProps {
+  type: ButtonType;
+  title: string;
+  styleType: StyleType;
+  size?: Size;
+  onClick?: () => void;
+  disabled?: boolean;
+  isLoading?: boolean;
+}
+
+const ButtonBox = styled.button`
+  padding: ${({ styleType }) => (styleType === 'black' ? '16px 55px' : '16px 40px')};
+  background: ${({ styleType }) => (styleType === 'black' ? 'black' : 'white')};
+  font-family: ${({ theme }) => theme?.fontEuljiro};
+  font-size: ${({ size }) => (size === 'big' ? '20px' : '16px')};
+  border: 1px solid ${({ styleType, theme }) => (styleType === 'black' ? 'black' : theme?.colorTextBeige)};
+  color: ${({ styleType, theme }) => (styleType === 'black' ? 'white' : theme?.colorLineDark)};
+
+  ${({ theme }) => theme?.mobile} {
+    padding: 12px 30px;
+    font-size: 14px;
+  }
+`;
+
+const TextButton: FC<ButtonProps> = ({
+  title,
+  type,
+  size,
+  onClick,
+  disabled = false,
+  styleType,
+  isLoading,
+}: ButtonProps) => {
+  return (
+    <ButtonBox
+      type={type}
+      onClick={onClick}
+      isLoading={isLoading}
+      styleType={styleType}
+      size={size}
+      disabled={disabled ? true : ''}
+    >
+      {title}
+    </ButtonBox>
+  );
+};
+
+TextButton.defaultProps = {
+  onClick: () => {},
+  disabled: false,
+  isLoading: false,
+  size: 'big',
+};
+
+export default TextButton;
