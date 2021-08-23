a1d916a0767490552e6b7e452cd52df45c8d3df2
yoonminsang <apdlyooapdl@naver.com>
2021-08-23 22:52:12 +0900

    feat: 주문목록 컨테이너에 리덕스 연결 및 기본 기능
    - post로 요청하는 부분 미완성
    - 페이지네이션부분은 리베이스 받고 수정

--- client/src/containers/my-order-list-container.tsx
+++ client/src/containers/my-order-list-container.tsx
@@ -2,13 +2,40 @@ import InquiryPeriod from 'components/my/inquiry-period';
 import MyBar from 'components/my/my-bar';
 import MyOrderList from 'components/my/my-order-list';
 import MyStatusBar from 'components/my/my-status-bar';
-import React, { FC, useState } from 'react';
+import { useHistory } from 'lib/router';
+import React, { FC, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from 'store';
+import { getOrders } from 'store/order';
 import { getLastMonth, getLastThreeMonth, getLastWeek, getToday } from 'utils/date';
 
 const MyOrderListContainer: FC = () => {
   const [prevDate, setPrevDate] = useState('');
   const [currentDate, setCurrentDate] = useState('');
   const [select, setSelect] = useState<undefined | number>(undefined);
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const { loading, user, orders, pageCount, totalCount, userLoading } = useSelector(
+    ({ auth, loading, order }: RootState) => ({
+      loading: loading['order/getListOrder'],
+      user: auth.user.userId,
+      orders: order.list.orders,
+      pageCount: order.list.pageCount,
+      totalCount: order.list.totalCount,
+      userLoading: loading['auth/getUser'],
+    }),
+  );
+
+  useEffect(() => {
+    // TODO: 페이지네이션 리베이스 받고 수정
+    dispatch({ type: getOrders.type, payload: { pageId: 1, prevDate: getLastThreeMonth(), currentDate: getToday() } });
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!userLoading && !user) history.push('/');
+  }, [userLoading, user, history]);
+
   const today = getToday();
   const onClickToday = () => {
     setPrevDate(today);
@@ -32,8 +59,11 @@ const MyOrderListContainer: FC = () => {
   };
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // TODO: dispatch prevDate, currentDate
+    // TODO: 페이지네이션 리베이스 받고 수정
+    dispatch({ type: getOrders.type, payload: { pageId: 1, prevDate, currentDate } });
   };
+
+  if (userLoading) return null;
   return (
     <>
       <MyBar />
@@ -51,7 +81,7 @@ const MyOrderListContainer: FC = () => {
         onSubmit={onSubmit}
       />
       <MyStatusBar data={['주문일자', '상품명', '상품일금액/수량', '주문상태']} />
-      <MyOrderList />
+      <MyOrderList loading={loading} orders={orders} pageCount={pageCount} totalCount={totalCount} />
     </>
   );
 };
