cab0af79e9c6b9d6c66f04fdab598c7b7ce5a288
yoonminsang <apdlyooapdl@naver.com>
2021-08-23 22:48:02 +0900

    feat: order 타입 지정

---
+++ client/src/types/order.ts
@@ -0,0 +1,17 @@
+export interface IOrder {
+  createdAt: string;
+  thumbnail: string;
+  title: string;
+  status: string;
+  price: number;
+  count: number;
+}
+
+export interface IOrderList {
+  orders: IOrder[];
+  totalCount: number;
+  pageCount: number;
+}
+export interface IOrderState {
+  pageId?: number;
+}
