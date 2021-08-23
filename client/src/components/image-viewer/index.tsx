fa55b6398a8068b67bdf77223216ee4c106f8ff2
Seogeurim <geulims@naver.com>
2021-08-23 21:55:13 +0900

    feat: 이미지 뷰어 컴포넌트 개발
    이미지를 확대하여 볼 수 있는 이미지 뷰어 컴포넌트 개발

---
+++ client/src/components/image-viewer/index.tsx
@@ -0,0 +1,111 @@
+import React, { FC, useCallback, useRef, MouseEvent } from 'react';
+import styled from 'lib/woowahan-components';
+
+interface ImageViewerProps {
+  imgSrc: string;
+  imgWidth: number;
+  imgHeight: number;
+}
+
+const Wrapper = styled.div`
+  position: relative;
+
+  .image-lens {
+    position: absolute;
+    border: 1px solid ${props => props.theme?.colorGreyLight};
+    background-color: rgba(255, 255, 255, 0.3);
+    width: 200px;
+    height: 200px;
+  }
+
+  .image-zoom {
+    position: absolute;
+    top: 0;
+    left: ${props => props.left as string}px;
+    width: ${props => props.imgWidth as string}px;
+    height: ${props => props.imgWidth as string}px;
+    background-image: url(${props => props.targetImg as string});
+    background-size: ${props => props.resultSize as string};
+    background-repeat: none;
+  }
+`;
+
+const ImageViewer: FC<ImageViewerProps> = ({ imgSrc, imgWidth, imgHeight }) => {
+  const imgRef = useRef<HTMLImageElement>(null);
+  const lensRef = useRef<HTMLDivElement>(null);
+  const zoomRef = useRef<HTMLDivElement>(null);
+
+  const getCursorPos = (e: MouseEvent): { x: number; y: number } => {
+    if (!imgRef.current) return { x: 0, y: 0 };
+
+    const imgClient = imgRef.current.getBoundingClientRect();
+
+    const x = e.pageX - imgClient.left - window.pageXOffset;
+    const y = e.pageY - imgClient.top - window.pageYOffset;
+
+    return { x, y };
+  };
+
+  const moveLens = useCallback((e: MouseEvent) => {
+    if (!imgRef.current || !lensRef.current || !zoomRef.current) return;
+    e.preventDefault();
+
+    const img = imgRef.current;
+    const lens = lensRef.current;
+    const zoom = zoomRef.current;
+
+    const cursorPos = getCursorPos(e);
+
+    if (cursorPos.x > img.width - 30 || cursorPos.x < 30 || cursorPos.y > img.height - 30 || cursorPos.y < 30) {
+      lens.style.display = 'none';
+      zoom.style.display = 'none';
+    } else {
+      lens.style.display = 'block';
+      zoom.style.display = 'block';
+      let x = cursorPos.x - lens.offsetWidth / 2;
+      let y = cursorPos.y - lens.offsetHeight / 2;
+
+      if (x > img.width - lens.offsetWidth) x = img.width - lens.offsetWidth;
+      if (x < 0) x = 0;
+      if (y > img.height - lens.offsetHeight) y = img.height - lens.offsetHeight;
+      if (y < 0) y = 0;
+
+      lensRef.current.style.left = `${x}px`;
+      lensRef.current.style.top = `${y}px`;
+
+      const cx = zoom.offsetWidth / lens.offsetWidth;
+      const cy = zoom.offsetHeight / lens.offsetHeight;
+
+      zoom.style.backgroundPosition = `-${x * cx}px -${y * cy}px`;
+    }
+  }, []);
+
+  const calcBgSize = () => {
+    if (!zoomRef.current || !lensRef.current) return '';
+
+    const zoom = zoomRef.current;
+    const lens = lensRef.current;
+
+    const cx = zoom.offsetWidth / lens.offsetWidth;
+    const cy = zoom.offsetHeight / lens.offsetHeight;
+
+    return `${imgWidth * cx}px ${imgHeight * cy}px`;
+  };
+
+  return (
+    <Wrapper left={imgWidth + 5} imgWidth={imgWidth} imgHeight={imgHeight} targetImg={imgSrc} resultSize={calcBgSize()}>
+      <img
+        ref={imgRef}
+        src={imgSrc}
+        alt="item-img"
+        width={`${imgWidth}px`}
+        height={`${imgHeight}px`}
+        onMouseMove={moveLens}
+      />
+      <div ref={lensRef} className="image-lens" onMouseMove={moveLens} />
+      <div ref={zoomRef} className="image-zoom" />
+    </Wrapper>
+  );
+};
+
+export default ImageViewer;
