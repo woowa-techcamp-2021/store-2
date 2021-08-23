dcf46fdde3055caed6ae3ed1d579855e17b0d2d5
negu63 <dnjsdn2019@gmail.com>
2021-08-23 22:00:44 +0900

    feat: 리스트 아이템 post 요청으로 변경

--- server/src/routes/items/index.ts
+++ server/src/routes/items/index.ts
@@ -5,6 +5,6 @@ import { getItems, getMainItems, getItem } from 'controllers/items';
 const router = Router();
 router.post('/main', getMainItems);
 router.get('/:id', getItem);
-router.get('/', getItems);
+router.post('/', getItems);
 
 export default router;
