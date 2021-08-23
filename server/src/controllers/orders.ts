85fef2e63b8d3304f94560fb2c94d7551601d351
yoonminsang <apdlyooapdl@naver.com>
2021-08-23 14:41:38 +0900

    refactor: 토큰 validation 서비스 계층 분리

--- server/src/controllers/orders.ts
+++ server/src/controllers/orders.ts
@@ -1,15 +1,12 @@
 import { Request, Response } from 'express';
 import ordersService from 'services/orders';
 import errorHandler from 'utils/error/error-handler';
-// import { decodeToken, getAccessToken } from 'utils/jwt';
+import { getAccessToken } from 'utils/jwt';
 
 export const getOrders = async (req: Request, res: Response): Promise<void> => {
-  // TODO: 프론트 연동 후 주석 갈아끼우기
-  // const token = getAccessToken(req.headers.authorization);
-  // const { uid } = decodeToken('access', token);
-  const uid = 'c50f7860-aa58-440f-825a-37aaa04b8182';
+  const token = getAccessToken(req.headers.authorization);
   try {
-    const orders = await ordersService.getOrders(uid);
+    const orders = await ordersService.getOrders(token);
     res.status(200).json(orders);
   } catch (err) {
     console.log(err);
--- server/src/services/orders.ts
+++ server/src/services/orders.ts
@@ -1,8 +1,18 @@
 import orderRepisitory from 'repositories/orders';
 import { OrderAttributes, OrderCreationAttributes } from 'models/order';
 import { Model } from 'sequelize';
+import { decodeToken } from 'utils/jwt';
+import errorGenerator from 'utils/error/error-generator';
 
-async function getOrders(uid: string): Promise<Model<OrderAttributes, OrderCreationAttributes>[]> {
+async function getOrders(token: string): Promise<Model<OrderAttributes, OrderCreationAttributes>[]> {
+  if (!token) {
+    throw errorGenerator({
+      message: 'GET /api/orders - no token',
+      code: 'req/no-token',
+    });
+  }
+
+  const { uid } = decodeToken('access', token);
   const orders = await orderRepisitory.getUserOrders(uid);
   return orders;
 }
