29540fb4204df54cb1b0d16d411daa525b70b9f8
yoonminsang <apdlyooapdl@naver.com>
2021-08-23 22:49:51 +0900

    wip: order 리덕스에 연결 및 로딩추가

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -4,19 +4,21 @@ import { authReducer, authSaga } from './auth';
 import { itemReducer, itemSaga } from './item';
 import { loadingReducer } from './loading';
 import { categoryReducer, categorySaga } from './category';
+import { orderReducer, orderSaga } from './order';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   loading: loadingReducer,
   item: itemReducer,
   category: categoryReducer,
+  order: orderReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
 
 export function* rootSaga(): Generator {
   try {
-    yield all([authSaga(), itemSaga(), categorySaga()]);
+    yield all([authSaga(), itemSaga(), categorySaga(), orderSaga()]);
   } catch (e) {
     throw new Error(e);
   }
