b41a3cada0d2fa93d18e27e84cf92dc9c6cb353f
negu63 <dnjsdn2019@gmail.com>
2021-08-23 15:53:43 +0900

    fix: 글로벌 스타일 폰트 수정

--- client/src/styles/theme.tsx
+++ client/src/styles/theme.tsx
@@ -84,6 +84,7 @@ const GlobalStyle = createGlobalStyle`
   #root {
     width: 100%;
     height: 100vh;
+    font-family: "Noto Sans KR", sans-serif;
   }
 
   a {
@@ -98,10 +99,6 @@ const GlobalStyle = createGlobalStyle`
   }
   button {
     cursor: pointer;
-  }
-
-  div {
-    font-family: "Noto Sans KR", sans-serif;
   }
 `;
 
