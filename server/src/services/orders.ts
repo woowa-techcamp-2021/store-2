90fbf7187a83a3ad10f1e53581a04df92f1e7a0d
edegiil <electropark917@gmail.com>
2021-08-24 23:00:40 +0900

    feat: 주문하기 API service 함수

--- server/src/services/orders.ts
+++ server/src/services/orders.ts
@@ -4,6 +4,18 @@ async function getOrders(uid: string, pageId = 1, prevDate: string, currentDate:
   return orderRepisitory.getUserOrders(uid, pageId, prevDate, currentDate);
 }
 
+async function postOrder(
+  uid: string,
+  phone: string,
+  receiver: string,
+  address: string,
+  itemId: number,
+  quantity: number,
+): Promise<void> {
+  await orderRepisitory.postOrder(uid, phone, receiver, address, itemId, quantity);
+}
+
 export default {
   getOrders,
+  postOrder,
 };
