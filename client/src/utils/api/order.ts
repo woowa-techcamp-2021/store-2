af5e1d6dda7b6bcb31ddfcca9ef4364340fa2d5f
yoonminsang <apdlyooapdl@naver.com>
2021-08-24 17:12:16 +0900

    fix: 주문목록 불러오기 post에서 get으로 수정

--- client/src/utils/api/order.ts
+++ client/src/utils/api/order.ts
@@ -3,5 +3,5 @@ import { IOrderList, IOrderState } from 'types/order';
 import client from './client';
 
 export const getOrderList = ({ pageId, prevDate, currentDate }: IOrderState): Promise<AxiosResponse> => {
-  return client.post<IOrderList>(`/api/orders?pageId=${pageId || 1}`, { prevDate, currentDate });
+  return client.get<IOrderList>(`/api/orders?pageId=${pageId || 1}&prevDate=${prevDate}&currentDate=${currentDate}`);
 };
