b348f9c9506361f354cd3d08576f4467da9f5d29
yoonminsang <apdlyooapdl@naver.com>
2021-08-24 16:34:12 +0900

    wip: 토큰 제너레이터 삭제

--- server/src/services/orders.ts
+++ server/src/services/orders.ts
@@ -1,15 +1,7 @@
 import orderRepisitory, { IOrdersData } from 'repositories/orders';
 import { decodeToken } from 'utils/jwt';
-import errorGenerator from 'utils/error/error-generator';
 
 async function getOrders(token: string, pageId = 1, prevDate: string, currentDate: string): Promise<IOrdersData> {
-  if (!token) {
-    throw errorGenerator({
-      message: 'GET /api/orders - no token',
-      code: 'req/no-token',
-    });
-  }
-
   const { uid } = decodeToken('access', token);
   const { orders, totalCount, pageCount } = await orderRepisitory.getUserOrders(uid, pageId, prevDate, currentDate);
 
