63b1275617069af12aedee64d008d71d48101028
Seogeurim <geulims@naver.com>
2021-08-24 11:48:16 +0900

    refactor: 마이페이지 네브바 정보 상수화

--- client/src/components/my/my-nav.tsx
+++ client/src/components/my/my-nav.tsx
@@ -1,7 +1,7 @@
-import { ADDRESS_URL, ORDER_LIST_URL, REVIEW_URL } from 'constants/urls';
-import { Link, useHistory } from 'lib/router';
-import styled from 'lib/woowahan-components';
 import React, { FC } from 'react';
+import styled from 'lib/woowahan-components';
+import { Link, useHistory } from 'lib/router';
+import { MY_PAGE_NAV } from 'constants/index';
 
 const Wrapper = styled.nav`
   margin-top: 20px;
@@ -29,14 +29,10 @@ const Wrapper = styled.nav`
 
 const MyNav: FC = () => {
   const { currentPath } = useHistory();
-  const myPageNav = [
-    { link: ORDER_LIST_URL, name: '주문목록 조회' },
-    { link: ADDRESS_URL, name: '배송지 관리' },
-    { link: REVIEW_URL, name: '나의 후기' },
-  ];
+
   return (
     <Wrapper>
-      {myPageNav.map(({ link, name }) => (
+      {MY_PAGE_NAV.map(({ link, name }) => (
         <Link key={name} to={link} className={link === currentPath ? 'active' : ''}>
           {name}
         </Link>
