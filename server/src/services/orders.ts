c5392ce1e77f6212a48803fe17a8825f5121dd24
yoonminsang <apdlyooapdl@naver.com>
2021-08-24 17:14:25 +0900

    refactor: 함수 바로 리턴

--- server/src/services/orders.ts
+++ server/src/services/orders.ts
@@ -1,9 +1,7 @@
 import orderRepisitory, { IOrdersData } from 'repositories/orders';
 
 async function getOrders(uid: string, pageId = 1, prevDate: string, currentDate: string): Promise<IOrdersData> {
-  const { orders, totalCount, pageCount } = await orderRepisitory.getUserOrders(uid, pageId, prevDate, currentDate);
-
-  return { orders, totalCount, pageCount };
+  return orderRepisitory.getUserOrders(uid, pageId, prevDate, currentDate);
 }
 
 export default {
