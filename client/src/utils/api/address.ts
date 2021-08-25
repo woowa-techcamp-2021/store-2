5107c2537fa9d23754844fa8934988c3af0fb384
yoonminsang <apdlyooapdl@naver.com>
2021-08-25 21:53:42 +0900

    feat: 주소 추가하기 api

--- client/src/utils/api/address.ts
+++ client/src/utils/api/address.ts
@@ -1,5 +1,8 @@
 import { AxiosResponse } from 'axios';
-import { IListAddress } from 'types/address';
+import { IListAddress, IAddressState } from 'types/address';
 import client from './client';
 
 export const getAddress = (): Promise<AxiosResponse> => client.get<IListAddress[]>('/api/address');
+
+export const addAddress = ({ name, receiver, address }: IAddressState): Promise<AxiosResponse> =>
+  client.post<IListAddress[]>('/api/address', { name, receiver, address });
