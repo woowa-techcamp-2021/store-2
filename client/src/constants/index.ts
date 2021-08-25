8fafcad35a32ee2c4defa2dd7afb3afbec72f5b4
yoonminsang <apdlyooapdl@naver.com>
2021-08-25 14:43:36 +0900

    wip: 에러 상수분리

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -8,6 +8,8 @@ export const SMART_MENU_LARGE_WIDTH = 1200;
 export const SMART_MENU_SMALL_WIDTH = 600;
 export const SMART_MENU_BLOCK_DELAY = 100;
 
+export const INNER_ERROR = '알 수 없는 에러가 발생했습니다.';
+
 export const GITHUB_LOGIN_LINK = `http://${window.location.hostname}:3000/api/auth/github`;
 
 export const SORT = [
