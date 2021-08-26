cc25c91efa7fa7168704f894f746350d38f7502f
Seogeurim <geulims@naver.com>
2021-08-26 14:56:23 +0900

    chore: seeder 환경설정
    - seeder config 파일 추가
    - sequelizerc 파일 추가

---
+++ server/src/config/db-seeder-config.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    timezone: '+09:00',
+  },
+};
