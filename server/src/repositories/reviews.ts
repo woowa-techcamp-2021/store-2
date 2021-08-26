3de85b35ea6e8c79ec8746d31dbbb243ad69a375
edegiil <electropark917@gmail.com>
2021-08-26 15:44:37 +0900

    feat: 후기 불러오기 API repository 함수

--- server/src/repositories/reviews.ts
+++ server/src/repositories/reviews.ts
@@ -1,4 +1,14 @@
 import { db } from 'models';
+import { Model, Sequelize } from 'sequelize';
+import { ReviewAttribures, ReviewCreationAttributes } from 'models/review';
+
+import errorGenerator from 'utils/error/error-generator';
+
+interface ReviewData extends ReviewAttribures {
+  userId: string;
+}
+
+const LIMIT_COUNT = 10;
 
 const postReview = async (
   uid: string,
@@ -18,4 +28,40 @@ const postReview = async (
   });
 };
 
-export default { postReview };
+const getReviews = async (
+  itemId: number,
+  pageId: number,
+): Promise<{ reviewData: Model<ReviewData, ReviewCreationAttributes>[]; totalCount: number }> => {
+  const reviewData = (await db.Review.findAll({
+    attributes: ['title', 'contents', 'imgUrl', 'score', [Sequelize.col('User.userId'), 'userId']],
+    where: {
+      ItemId: itemId,
+    },
+    order: [['createdAt', 'DESC']],
+    limit: LIMIT_COUNT,
+    offset: (pageId - 1) * LIMIT_COUNT,
+    include: [
+      {
+        model: db.User,
+        attributes: ['userId'],
+      },
+    ],
+  })) as Model<ReviewData, ReviewCreationAttributes>[];
+
+  const totalCount = await db.Review.count({
+    where: {
+      ItemId: itemId,
+    },
+  });
+
+  if (!reviewData) {
+    throw errorGenerator({
+      message: 'GET /api/reviews - reviews not found',
+      code: 'reviews/reviews-not-found',
+    });
+  }
+
+  return { reviewData, totalCount };
+};
+
+export default { postReview, getReviews };
