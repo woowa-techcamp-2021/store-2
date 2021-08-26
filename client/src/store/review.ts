df5294eaf72ea08884d9a8c286092f747fba375f
yoonminsang <apdlyooapdl@naver.com>
2021-08-26 15:15:10 +0900

    feat: 리뷰 스토어 생성

---
+++ client/src/store/review.ts
@@ -0,0 +1,46 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { takeLatest } from 'redux-saga/effects';
+import { getReviewsSaga, postReviewSaga } from 'saga/reviews';
+import { IListReview } from 'types/review';
+
+interface StateProps {
+  list: IListReview;
+}
+
+export const initialState: StateProps = {
+  list: {
+    reviews: [],
+    pageCount: 0,
+    totalCount: 0,
+  },
+};
+
+const reviewSlice = createSlice({
+  name: 'review',
+  initialState,
+  reducers: {
+    getReviews: state => state,
+    getReviewsSuccess: (state, action: PayloadAction<IListReview>) => {
+      state.list = action.payload;
+      return state;
+    },
+    getReviewsFail: state => state,
+    postReview: state => state,
+    postReviewSuccess: (state, action: PayloadAction<IListReview>) => {
+      state.list = action.payload;
+      return state;
+    },
+    postReviewsFail: state => state,
+  },
+});
+
+const { actions, reducer: reviewReducer } = reviewSlice;
+
+export const { getReviews, getReviewsSuccess, getReviewsFail, postReview, postReviewSuccess, postReviewsFail } =
+  actions;
+export { reviewReducer };
+
+export function* reviewSaga(): Generator {
+  yield takeLatest(getReviews, getReviewsSaga);
+  yield takeLatest(postReview, postReviewSaga);
+}
