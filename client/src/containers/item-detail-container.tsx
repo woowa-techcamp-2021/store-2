8a96d9257eeaa9d588e6d5c397efd3f0f5a60a15
edegiil <electropark917@gmail.com>
2021-08-26 13:05:15 +0900

    fix: 제품 상세 페이지에서 로그인이 되어 있지 않으면 모달 적용

--- client/src/containers/item-detail-container.tsx
+++ client/src/containers/item-detail-container.tsx
@@ -4,6 +4,7 @@ import { useParams, useHistory } from 'lib/router';
 
 import ItemInfo from 'components/item-detail/item-info';
 import Detail from 'components/item-detail/detail';
+import { Modal } from 'components';
 
 import { RootState } from 'store';
 import { getItem } from 'store/item';
@@ -12,20 +13,24 @@ import { PAYMENT_URL } from 'constants/urls';
 
 const MainItemContainer: FC = () => {
   const [count, setCount] = useState(1);
+  const [modalVisible, setModalVisible] = useState(false);
   const dispatch = useDispatch();
   const history = useHistory();
   const { id } = useParams();
 
-  const { thumbnail, title, price, contents, isLike, isSoldOut, reviewCount } = useSelector(({ item }: RootState) => ({
-    thumbnail: item.item.thumbnail,
-    title: item.item.title,
-    price: item.item.price,
-    contents: item.item.contents,
-    salePercent: item.item.salePercent,
-    isLike: item.item.isLike,
-    isSoldOut: item.item.isSoldOut,
-    reviewCount: item.item.reviewCount,
-  }));
+  const { thumbnail, title, price, contents, isLike, isSoldOut, reviewCount, userId } = useSelector(
+    ({ item, auth }: RootState) => ({
+      thumbnail: item.item.thumbnail,
+      title: item.item.title,
+      price: item.item.price,
+      contents: item.item.contents,
+      salePercent: item.item.salePercent,
+      isLike: item.item.isLike,
+      isSoldOut: item.item.isSoldOut,
+      reviewCount: item.item.reviewCount,
+      userId: auth.user.userId,
+    }),
+  );
 
   useEffect(() => {
     dispatch({ type: getItem.type, payload: { id } });
@@ -36,8 +41,12 @@ const MainItemContainer: FC = () => {
   };
 
   const onBuy = () => {
-    window.sessionStorage.setItem('order', `${id}-${count}`);
-    history.push(PAYMENT_URL);
+    if (userId) {
+      window.sessionStorage.setItem('order', `${id}-${count}`);
+      history.push(PAYMENT_URL);
+    } else {
+      setModalVisible(true);
+    }
   };
 
   return (
@@ -53,6 +62,7 @@ const MainItemContainer: FC = () => {
         setCount={setCount}
       />
       <Detail contents={contents} reviewCount={reviewCount} />
+      <Modal type="alert" body="로그인이 필요합니다" visible={modalVisible} setVisible={setModalVisible} />
     </>
   );
 };
