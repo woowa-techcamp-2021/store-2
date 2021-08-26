6a5647ef08690ce8076e73e5021042aa159e6c4c
negu63 <dnjsdn2019@gmail.com>
2021-08-26 15:03:28 +0900

    feat: 장바구니 상품 파싱 함수

---
+++ client/src/utils/cart-generator.ts
@@ -0,0 +1,36 @@
+import { CartItem } from 'types/cart';
+import { cartValidator } from './validation/cart-validator';
+
+const cartGenerator = (): CartItem[] => {
+  if (cartValidator()) {
+    const cartItems: CartItem[] = [];
+    const data = localStorage.getItem('cart') as string;
+    const cartData = data.split(',');
+    cartData.forEach((value, idx) => {
+      const num = Math.floor(idx / 5);
+      switch (idx % 5) {
+        case 0:
+          cartItems.push({ id: value, thumbnail: '', title: '', count: 0, price: 0 });
+          break;
+        case 1:
+          cartItems[num].thumbnail = value;
+          break;
+        case 2:
+          cartItems[num].title = value;
+          break;
+        case 3:
+          cartItems[num].count = Number(value);
+          break;
+        case 4:
+          cartItems[num].price = Number(value);
+          break;
+        default:
+          break;
+      }
+    });
+    return cartItems;
+  }
+  return [];
+};
+
+export { cartGenerator };
