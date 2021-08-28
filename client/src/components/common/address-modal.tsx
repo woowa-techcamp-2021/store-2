667a0fac6e7b07bc94e1121499dba1332fd9cff2
yoonminsang <apdlyooapdl@naver.com>
2021-08-28 17:19:27 +0900

    feat: 라이브러리를 감싸는 모달 컴포넌트 생성
    - 재사용하면 좋은데 형식이 너무 달라서 할 수가 없었음
    - 이런부분때문에 처음에 팀원이 모달창을 그렇게 만드는걸 추천하지 않았던 것
    - 물론 타입넘겨줘서 재사용하는건 매우 좋은 방법중한가지라고 생각

---
+++ client/src/components/common/address-modal.tsx
@@ -0,0 +1,47 @@
+import styled from 'lib/woowahan-components';
+import React, { FC } from 'react';
+import DaumPostcode from 'react-daum-postcode';
+
+interface IAddressData {
+  roadAddress: string;
+}
+
+interface AddressModalProps {
+  handleComplete: (data: IAddressData) => void;
+  setModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const Modal = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+`;
+
+const ModalInner = styled.div`
+  width: 80%;
+  #__daum__layer_1 {
+    border-radius: 10px;
+  }
+`;
+
+const AddressModal: FC<AddressModalProps> = ({ handleComplete, setModal }) => {
+  const modalClickHandler = (e: Event) => {
+    if (e.target === e.currentTarget) setModal(false);
+  };
+  return (
+    <Modal onClick={modalClickHandler}>
+      <ModalInner>
+        <DaumPostcode onComplete={handleComplete} animation width="90%" />
+      </ModalInner>
+    </Modal>
+  );
+};
+
+export default AddressModal;
