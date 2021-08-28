4a68c9d944632c1e31593e39732597121853bc76
yoonminsang <apdlyooapdl@naver.com>
2021-08-28 17:20:21 +0900

    feat: 주소폼에 라이브러리 적용하기 위해 수정

--- client/src/components/my/my-address/my-address-form.tsx
+++ client/src/components/my/my-address/my-address-form.tsx
@@ -1,20 +1,28 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import styled from 'lib/woowahan-components';
 
 import GridForm from 'components/common/grid-form';
 import TextButton from 'components/common/button/text-button';
+import AddressModal from 'components/common/address-modal';
 
 interface MyAddressFormProps {
   name: string;
   receiver: string;
   address: string;
+  setAddress: React.Dispatch<React.SetStateAction<string>>;
+  addressDetail: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onSubmit: (e: React.FormEvent) => void;
   nameError: string;
   receiverError: string;
   addressError: string;
+  addressDetailError: string;
   addError: string;
   loading: boolean;
+}
+
+interface IAddressData {
+  roadAddress: string;
 }
 
 const InputWrapper = styled.div`
@@ -46,35 +54,64 @@ const MyAddressForm: FC<MyAddressFormProps> = ({
   name,
   receiver,
   address,
+  setAddress,
+  addressDetail,
   onChange,
   onSubmit,
   nameError,
   receiverError,
   addressError,
+  addressDetailError,
   addError,
   loading,
 }) => {
+  const [modal, setModal] = useState(false);
+  const handleComplete = (data: IAddressData) => {
+    setAddress(data.roadAddress);
+  };
+  useEffect(() => {
+    if (address) setModal(false);
+  }, [address]);
   return (
-    <form onSubmit={onSubmit}>
-      <GridForm titles={['배송지이름*', '받는분*', '주소*']}>
-        <InputWrapper>
-          <input name="name" value={name} onChange={onChange} maxLength={10} />
-          <InputErrorMessage>{nameError}</InputErrorMessage>
-        </InputWrapper>
-        <InputWrapper>
-          <input name="receiver" value={receiver} onChange={onChange} maxLength={20} />
-          <InputErrorMessage>{receiverError}</InputErrorMessage>
-        </InputWrapper>
-        <InputWrapper>
-          <input name="address" value={address} onChange={onChange} maxLength={50} />
-          <InputErrorMessage>{addressError}</InputErrorMessage>
-        </InputWrapper>
-      </GridForm>
-      <ButtonWrapper>
-        <div>{addError}</div>
-        <TextButton type="submit" styleType="black" title="배송지 추가" size="tiny" isLoading={loading} />
-      </ButtonWrapper>
-    </form>
+    <>
+      <form onSubmit={onSubmit}>
+        <GridForm titles={['배송지이름*', '받는분*', '주소*', '상세주소*']}>
+          <InputWrapper>
+            <input name="name" value={name} onChange={onChange} maxLength={10} placeholder="우형집" />
+            <InputErrorMessage>{nameError}</InputErrorMessage>
+          </InputWrapper>
+          <InputWrapper>
+            <input name="receiver" value={receiver} onChange={onChange} maxLength={20} placeholder="홍길동" />
+            <InputErrorMessage>{receiverError}</InputErrorMessage>
+          </InputWrapper>
+          <InputWrapper>
+            <input
+              value={address}
+              onChange={onChange}
+              readOnly
+              onClick={() => setModal(true)}
+              placeholder="서울 강남구 가로수길"
+            />
+            <InputErrorMessage>{addressError}</InputErrorMessage>
+          </InputWrapper>
+          <InputWrapper>
+            <input
+              name="addressDetail"
+              value={addressDetail}
+              onChange={onChange}
+              readOnly={!address.length}
+              placeholder="102호"
+            />
+            <InputErrorMessage>{addressDetailError}</InputErrorMessage>
+          </InputWrapper>
+        </GridForm>
+        <ButtonWrapper>
+          <div>{addError}</div>
+          <TextButton type="submit" styleType="black" title="배송지 추가" size="tiny" isLoading={loading} />
+        </ButtonWrapper>
+      </form>
+      {modal && <AddressModal handleComplete={handleComplete} setModal={setModal} />}
+    </>
   );
 };
 
